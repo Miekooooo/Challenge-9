df2f0d06ebab666c1bb1e268795d984c5a09897f
miekoooo <mocket17@gmail.com>
2023-10-25 18:49:42 -0500

    finished index and generatemarkdown files

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,112 @@
-// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Array of questions for user input
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "Enter the project title:",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Provide a project description:",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Enter installation instructions:",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Provide usage information:",
+  },
+  {
+    type: "input",
+    name: "contribution",
+    message: "Provide contribution guidelines:",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "Provide test instructions:",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Choose a license for your application:",
+    choices: ["MIT", "GNU GPLv3", "Apache 2.0", "ISC", "None"],
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "Enter your GitHub username:",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Enter your email address:",
+  },
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Function to generate README content
+function generateReadme(answers) {
+  // Construct the README content based on user answers
+  return `
+# ${answers.title}
 
-// TODO: Create a function to initialize app
-function init() {}
+## Description
+${answers.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+## License
+This project is licensed under the ${answers.license} license.
+
+## Contributing
+${answers.contribution}
+
+## Tests
+${answers.tests}
+
+## Questions
+- GitHub: [${answers.github}](https://github.com/${answers.github})
+- Email: ${answers.email}
+`;
+}
+
+// Function to write README file
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log("README.md has been generated.");
+    }
+  });
+}
+
+// Function to initialize app
+function init() {
+  inquirer.prompt(questions).then((answers) => {
+    const readmeContent = generateReadme(answers);
+    writeToFile("README.md", readmeContent);
+  });
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,85 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (!license) {
+    return "";
+  }
+  // Define badge URLs for different licenses, you can add more if needed.
+  const licenseBadges = {
+    MIT: "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
+    "GNU GPLv3": "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)",
+    "Apache 2.0": "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://www.apache.org/licenses/LICENSE-2.0)",
+    ISC: "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)",
+  };
+
+  return licenseBadges[license];
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (!license) {
+    return "";
+  }
+  // Define license URLs
+  const licenseLinks = {
+    MIT: "https://opensource.org/licenses/MIT",
+    "GNU GPLv3": "https://www.gnu.org/licenses/gpl-3.0",
+    "Apache 2.0": "https://www.apache.org/licenses/LICENSE-2.0",
+    ISC: "https://opensource.org/licenses/ISC",
+  };
+
+  return `For details, please visit the [${license} License](${licenseLinks[license]}).`;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license) {
+    return "";
+  }
+
+  return `## License
+This project is licensed under the ${license} license. ${renderLicenseLink(license)}`;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  const licenseBadge = renderLicenseBadge(data.license);
+  const licenseSection = renderLicenseSection(data.license);
+
   return `# ${data.title}
 
+${licenseBadge}
+
+## Description
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+${licenseSection}
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+${licenseSection}
+
+## Contributing
+${data.contribution}
+
+## Tests
+${data.tests}
+
+## Questions
+- GitHub: [${data.github}](https://github.com/${data.github})
+- Email: ${data.email}
 `;
 }
 
